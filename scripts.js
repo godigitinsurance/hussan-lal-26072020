c4beb9587b3e0ed9eee61146c309844cdf5994a9
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-25 10:54:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Hussan Lal");
+$("#policyNumber").text("PQE6209415");
+$("#startDate").text("26-Jul-2020");
+$("#endDate").text("25-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CP8060");
+$("#make").text("BAJAJ");
+$("#model").text("PLATINA");
+$("#vehicalSubType").text("STD");
+$("#engineNo").text("DZZPEE27317");
+$("#chassis").text("MD2A18AZEPE26818");
